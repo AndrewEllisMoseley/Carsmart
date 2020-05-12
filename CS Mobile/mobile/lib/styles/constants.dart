211cf0e18a85b29aadfd00e3b44dd7e071ec516d
Andrew Ellis-Moseley <andrew.john.moseley@gmail.com>
2020-05-12 21:08:24 +0100

    update travis ci files

--- CS Mobile/mobile/lib/styles/constants.dart
+++ CS Mobile/mobile/lib/styles/constants.dart
@@ -8,7 +8,8 @@ const kLightGreyText = Color(0xFFDCDCDC);
 const kBlackText = Color(0xFF000000);
 const kWhiteText = Color(0xFFFFFFFF);
 
-kTextFieldInputDecoration(String labelText, String hintText, Icon icon) {
+dynamic kTextFieldInputDecoration(
+    String labelText, String hintText, Icon icon) {
   return InputDecoration(
     labelText: labelText,
     labelStyle: TextStyle(
