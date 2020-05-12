696d1ce19f23674e66c134f31f51ada51ec33cde
Andrew Ellis-Moseley <andrew.john.moseley@gmail.com>
2020-05-12 14:13:07 +0100

    add widget test for custom input field

--- CS Mobile/mobile/lib/widgets/atoms/cInputField.dart
+++ CS Mobile/mobile/lib/widgets/atoms/cInputField.dart
@@ -17,19 +17,23 @@ class CInputField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      // controller: usernameController,
-      keyboardType: TextInputType.text,
-      textInputAction: TextInputAction.next,
-      // focusNode: userFocus,
-      // onFieldSubmitted: (_) {
-      //   FocusScope.of(context).requestFocus(passFocus);
-      // },
-      // validator: (value) => emptyValidation(value),
-      decoration: kTextFieldInputDecoration(
-        fieldLabel,
-        fieldPlaceholder,
-        icon,
+    return Scaffold(
+      body: SafeArea(
+        child: TextFormField(
+          // controller: usernameController,
+          keyboardType: TextInputType.text,
+          textInputAction: TextInputAction.next,
+          // focusNode: userFocus,
+          // onFieldSubmitted: (_) {
+          //   FocusScope.of(context).requestFocus(passFocus);
+          // },
+          // validator: (value) => emptyValidation(value),
+          decoration: kTextFieldInputDecoration(
+            fieldLabel,
+            fieldPlaceholder,
+            icon,
+          ),
+        ),
       ),
     );
   }
--- CS Mobile/mobile/test/atoms/cInputField_widget_test.dart
+++ CS Mobile/mobile/test/atoms/cInputField_widget_test.dart
@@ -42,4 +42,43 @@ void main() {
     //  then
     expect(find.text('Email'), findsOneWidget);
   });
+
+  testWidgets("Atom - CInputField shown with an icon",
+      (WidgetTester tester) async {
+    //  given
+    final widget = CInputField(
+      fieldController: null,
+      fieldLabel: 'Email',
+      validator: null,
+      icon: Icon(Icons.email),
+    );
+
+    //  when
+    await tester.pumpWidget(_wrapWithMaterialApp(widget));
+
+    //  then
+    expect(find.byIcon(Icons.email), findsOneWidget);
+  });
+
+  testWidgets("Atom - CInputField shown with a placeholder",
+      (WidgetTester tester) async {
+    //  given
+    final widget = CInputField(
+      fieldController: null,
+      fieldLabel: 'Email',
+      validator: null,
+      icon: Icon(Icons.email),
+      fieldPlaceholder: 'Placeholder',
+    );
+
+    //  when
+    await tester.pumpWidget(_wrapWithMaterialApp(widget));
+
+    //  then
+    expect(
+      tester.widget(find.byType(CInputField)),
+      isA<CInputField>()
+          .having((t) => t.fieldPlaceholder, 'placeholder', 'Placeholder'),
+    );
+  });
 }
