8f9d2004a607393cd4dd61ef10267b4ed1622f6c
Andrew Ellis-Moseley <andrew.john.moseley@gmail.com>
2020-05-14 16:24:48 +0100

    revert back to landing page

--- CS Mobile/mobile/lib/main.dart
+++ CS Mobile/mobile/lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:mobile/screens/landing/land.dart';
 import 'package:mobile/screens/vehicle/vehicle_details_page.dart';
 
-// import 'screens/landing/landing_page.dart';
+import 'screens/landing/landing_page.dart';
 import 'screens/login/login_page.dart';
 import 'screens/register/register_one_page.dart';
 import 'screens/register/register_three_page.dart';
@@ -27,7 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(scaffoldBackgroundColor: Colors.white),
       initialRoute: '/',
       routes: {
-        '/': (context) => HomeView(),
+        '/': (context) => LandingPage(),
         '/login': (context) => LoginPage(),
         '/register-one': (context) => RegisterOnePage(),
         '/register-two': (context) => RegisterTwoPage(),
